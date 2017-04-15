f543687314b3ca50b6efd0b6af636e8faa80125b
Aitor Bernal <alu0100830064@ull.edu.es>
2017-04-15 01:41:58 +0100

    Subiendo algoritmo final

--- Diffie-Hellman.py
+++ Diffie-Hellman.py
@@ -1,31 +1,55 @@
 
 def in_alpha():
 	w = input("------Introduzca alpha: ")
-	w = w.split(',')
-	return w
+
+	return int(w)
 
 def in_module():
 	w = input("------Introduzca el modulo: ")
-	w = w.split(',')
-	return w
+
+	return int(w)
 
 def in_xb():
 	w = input("------Introduzca xB: ")
-	w = w.split(',')
-	return w
+
+	return int(w)
 
 def in_xa():
 	w = input("------Introduzca xA: ")
-	w = w.split(',')
-	return w
+
+	return int(w)
+
+def exponente(y,b,mod):
+    x = 1
+    while (b > 1):
+        if( b%2 == 0):
+            print (str(y)+" | "+str(b)+ " | "+str(x))
+            y = (y ** 2)%mod
+            b = b/2
+            print("es par")
+
+
+        if(b%2 == 1):
+            print (str(y)+" | "+str(b)+ " | "+str(x))
+            b -= 1
+            x = (x * y)%mod
+            print("es impar")
+
+    return x
+
+
+
+
+
 
 def calculo(alfa,xa,xb,modulo):
-    ya = (alfa ** xa)%modulo
-    yb = (alfa ** xb)%modulo
-    k1 = (yb ** xa)%modulo
-    k2 = (ya ** xb)%modulo
-    print(k1)
-    print(k2)
+
+    ya = exponente(alfa,xa,modulo)
+    yb = exponente(alfa,xb,modulo)
+    print (yb)
+    k1 = exponente(yb,xa,modulo)
+    k2 = exponente(ya, xb, modulo)
+
 
     if(k1 == k2):
         print ("p = "+str(modulo)+", a = "+str(alfa)+", xa = "+str(xa)+", xb = "+str(xb)+", ya = "+str(ya)+", yb = "+str(yb)+", k = "+str(k1))
@@ -33,4 +57,6 @@ def calculo(alfa,xa,xb,modulo):
         print("No son iguales")
 
 
-calculo(4,5,2,13);
+#calculo(in_alpha(),in_xa(),in_xb(),in_module());
+
+calculo(43,54,71,113);
